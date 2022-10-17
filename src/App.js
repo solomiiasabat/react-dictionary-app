f96da19599bf3191fadb287ba29182f930aefb78
solomiyasabat <solomiyasabat@gmail.com>
2022-10-17 15:56:20 +0300

    customized file for future coding

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced code
+          </a>{" "}
+          by Solomiia Sabat
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
