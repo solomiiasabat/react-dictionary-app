352d85ac5b5e6fedd06db4367e6319b00e39ce0b
solomiyasabat <solomiyasabat@gmail.com>
2022-10-18 17:18:25 +0300

    added API

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState("");
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
 
   function handleChange(event) {
