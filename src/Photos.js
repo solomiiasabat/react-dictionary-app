01e95270b3ba22fd6a1ef0cd59e96af31c087a8b
solomiyasabat <solomiyasabat@gmail.com>
2022-11-03 12:18:31 +0200

    improved photo display

--- src/Photos.js
+++ src/Photos.js
@@ -3,15 +3,16 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
+    console.log(props.photos);
     return (
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4" key={index}>
+              <div className="col-3" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
-                    src={photo.src.landscape}
+                    src={photo.src.portrait}
                     className="img-fluid"
                     alt={photo.alt}
                   />
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -17,7 +17,7 @@ export default function SearchEngine() {
 
     const apiPexelsKey =
       "563492ad6f917000010000017ddba45ac07c4923ade329071eb1bc2f";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=4`;
 
     let headers = { Authorization: `Bearer ${apiPexelsKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
